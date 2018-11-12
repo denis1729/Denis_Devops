44a0dd405028cdfe1ee78c663e5771b5cd44c455
denis1729 <denis_cc1729@hotmail.com>
2018-11-12 16:38:05 -0400

    add the environment

---
+++ Vagrant/environment.bat
@@ -0,0 +1,12 @@
+@echo off
+set OS_AUTH_URL=http://10.28.3.240:5000/v3
+set OS_IDENTITY_API_VERSION=3
+set OS_TENANT_NAME=4d0cb3e169a14806ba2a4b798104039a
+set OS_PROJECT_NAME=fj_deniscamacho_at07-desktop
+set OS_USERNAME=DenisCamacho
+set OS_PASSWORD=QWERTY12345qwerty
+set OS_DOMAIN_NAME=fundacion-jala
+set OS_FLAVOR=f1.desktop.large
+set OS_IMAGE=ubuntu-14.04-amd64-server_18062018
+set OS_KEY_PAIR_NAME=denis
+set OS_REGION_NAME=bootstack-jalasoft
