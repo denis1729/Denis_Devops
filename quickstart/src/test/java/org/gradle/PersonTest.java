11ca21f3b47dd1849f0d786c903b8b2a665fc90d
denis1729 <denis_cc1729@hotmail.com>
2018-11-14 17:34:56 -0400

    update vagrantfile and add unit test for class Person

--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,9 +1,10 @@
-package java.org.gradle;
+package org.gradle;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 
 public class PersonTest {
@@ -12,9 +13,10 @@ public class PersonTest {
 
     @Before
     public void before() {
-        person = new Person("Denis", "Camacho", 21);
+        person = new Person("Denis", "Camacho", 21, "6526103", "male");
     }
 
+    //positive
     @Test
     public void canConstructAPersonWithAName() {
         assertEquals("Denis", person.getName());
@@ -28,5 +30,41 @@ public class PersonTest {
     @Test
     public void canConstructAPersonWithAAge() {
         assertEquals(21, person.getAge());
+    }
+
+    @Test
+    public void canConstructAPersonWithAId() {
+        assertEquals("6526103", person.getId());
+    }
+
+    @Test
+    public void canConstructAPersonWithAGender() {
+        assertEquals("male", person.getGender());
+    }
+
+    //negative
+    @Test
+    public void canConstructAPersonWithANameNegative() {
+        assertFalse(person.getName().equals("Emma"));
+    }
+
+    @Test
+    public void canConstructAPersonWithALastNameNegative() {
+        assertFalse(person.getLastName().equals("Roman"));
+    }
+
+    @Test
+    public void canConstructAPersonWithAAgeNegative() {
+        assertFalse(person.getAge() == 22);
+    }
+
+    @Test
+    public void canConstructAPersonWithAIdNegative() {
+        assertFalse(person.getId().equals("6526104"));
+    }
+
+    @Test
+    public void canConstructAPersonWithAGenderNegative() {
+        assertFalse(person.getGender().equals("female"));
     }
 }
