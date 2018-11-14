777dfb62e23ada02e24dc69780bbc1746fd10416
denis1729 <denis_cc1729@hotmail.com>
2018-11-14 17:34:56 -0400

    update vagrantfile and add unit test for class Person

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -1,14 +1,18 @@
-package java.org.gradle;
+package org.gradle;
 
 public class Person {
     private final String name;
     private final String lastName;
+    private final String id;
+    private final String gender;
     private final int age;
 
-    public Person(String name, String lastName, int age) {
+    public Person(String name, String lastName, int age, String id, String gender) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
+        this.id = id;
+        this.gender = gender;
     }
 
     public String getName() {
@@ -19,7 +23,15 @@ public class Person {
         return lastName;
     }
 
+    public String getId() {
+        return id;
+    }
+
     public int getAge() {
         return age;
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
