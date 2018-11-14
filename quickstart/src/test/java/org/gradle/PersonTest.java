00a7e23d022e2d8368bda816da2f00ba564ef512
denis1729 <denis_cc1729@hotmail.com>
2018-11-14 15:29:35 -0400

    delete diretory build and .gradle

--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,12 +1,32 @@
-package org.gradle;
+package java.org.gradle;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class PersonTest {
+
+    private Person person;
+
+    @Before
+    public void before() {
+        person = new Person("Denis", "Camacho", 21);
+    }
+
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
-        assertEquals("Larry", person.getName());
+        assertEquals("Denis", person.getName());
+    }
+
+    @Test
+    public void canConstructAPersonWithALastName() {
+        assertEquals("Camacho", person.getLastName());
+    }
+
+    @Test
+    public void canConstructAPersonWithAAge() {
+        assertEquals(21, person.getAge());
     }
 }
